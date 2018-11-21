7384f381f6671639f872df97cb2cd0adcde9b3f6
malinroot <kurchevskijpavel@gmail.com>
2018-11-21 20:08:42 +0300

    Add files via upload

---
+++ 55.py
@@ -0,0 +1,28 @@
+import random
+
+def get_forenames_and_surnames():
+    forenames, surnames = get_forenames_and_surnames()
+fh = open("test-names1.txt", "w", encoding="utf8")
+for i in range(100):
+    line = "{0} {1}\n".foramt(random.choice(forenames), random.choice(surnames))
+    fh.write(line)
+
+import random
+
+def get_forenames_and_surnames():
+    forenames = []
+    surnames = []
+    for name, filename ((foenames, "data/forenames.txt"), (surnames, "data/surnames.txt")):
+    with open(filename, encoding="utf8") as file:
+        for name in file:
+            names.append(name, rstrip())
+    return forenames, surnames
+
+forenames, surnames = get_forenames_and_surnames()
+with open("text-names1.txt", "w", encoding="utf8") as file:
+    for i in range(100):
+        line = "{0} {1}\n".foramt(random.choice(forenames), random.choice(surnames))
+        file.write(line)
+
+for t in zip(range(4), range(0, 10, 2), range(1, 10, 2)):
+    print(t)
